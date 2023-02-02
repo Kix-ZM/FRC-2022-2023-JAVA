9a646d99e346e3f3b7649117f9e13ed3b4bd8ef4
TerminatingCoder101 <ganesha4us1@gmail.com>
2023-02-01 17:42:26 -0800

    Test
    Testing This Computer's Github as it had problems

--- Mallet/src/main/java/frc/robot/Constants.java
+++ Mallet/src/main/java/frc/robot/Constants.java
@@ -12,6 +12,7 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
+//Test
 public final class Constants {
     public static final double SPEED = 0.25;
     public static final double kCountsPerRevolution = 4096.0;
