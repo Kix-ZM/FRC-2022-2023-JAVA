679ee8e7c20a8436cad6040c8008f5abe12c2a0a
overlordpro-sys <64398319+overlordpro-sys@users.noreply.github.com>
2023-02-10 18:17:30 -0800

    Made entire thing work for driver station Need to test with roborio network tables

--- python-multiCameraServer/visionPi.py
+++ python-multiCameraServer/visionPi.py
@@ -4,8 +4,7 @@ import time
 import cv2
 import numpy as np
 from cscore import CameraServer
-from networktables import NetworkTables
-
+from ntcore import NetworkTableInstance
 
 def main():
     with open('/boot/frc.json') as f:
@@ -21,58 +20,52 @@ def main():
     output_stream = CameraServer.putVideo('Camera 0', width, height)
 
     # Initialize table for vision output
-    NetworkTables.initialize(server='roborio-4470-frc.local')  # need to test ip
-    vision_nt = NetworkTables.getTable('vision')
+    inst = NetworkTableInstance.getDefault()
+    vision_nt = inst.getTable("vision")
+    x_sub = vision_nt.getIntegerTopic("target_x").publish()
+    y_sub = vision_nt.getIntegerTopic("target_y").publish()
+    inst.startClient4("wpilibpi")
+    inst.setServerTeam(4470)
+    inst.startDSClient()
 
     # Allocating new images is very expensive, always try to preallocate
     img = np.zeros(shape=(height, width, 3), dtype=np.uint8)
 
-    # Wait for NetworkTables to start
+    # Wait for NetworkTable to start
     time.sleep(0.5)
 
     while True:
-        # Start time for frame
-        # start_time = time.time()
-
         # Grab frame from camera and make a copy of it
         frame_time, input_img = input_stream.grabFrame(img)
         output_img = np.copy(input_img)
 
-        # Notify output of error and skip iteration
-        if frame_time == 0:
-            output_stream.notifyError(input_stream.getError())
-            continue
-
         # Convert to HSV and threshold image for yellow cone
-        hsv_img = cv2.cvtColor(input_stream, cv2.COLOR_BGR2HSV)
+        hsv_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2HSV)
         yellow_mask = cv2.inRange(hsv_img, (18, 71, 167), (39, 255, 255))
 
-        contour_list, hierarchy = cv2.findContours(yellow_mask, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
+        contour_list, hierarchy = cv2.findContours(yellow_mask, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_SIMPLE)
         if len(contour_list) != 0:
             contour = max(contour_list, key=cv2.contourArea)
-            # Ignore small contours that could be because of noise/bad thresholding
-            if cv2.contourArea(contour) < 1000:
-                continue
+            if cv2.contourArea(contour) > 200:
+                # Draw contour on output image
+                cv2.drawContours(output_img, contour, -1, color=(255, 255, 255), thickness=-1)
 
-            # Draw contour on output image
-            cv2.drawContours(output_img, contour, -1, color=(255, 255, 255), thickness=-1)
+                m = cv2.moments(contour)
+                if m["m00"] != 0:
+                    x = int(m["m10"] / m["m00"])
+                    y = int(m["m01"] / m["m00"])
 
-            m = cv2.moments(contour)
-            if m["m00"] != 0:
-                x = int(m["m10"] / m["m00"])
-                y = int(m["m01"] / m["m00"])
-            else:
-                continue
+                    # Print center coordinates in console and send put on NetworkTable
+                    print("Center X: ", x, "| Center Y: ", y)
+                    x_sub.set(x)
+                    y_sub.set(y)
 
-            # Print center coordinates in console and send put on NetworkTable
-            print("Center X: ", x, "| Center Y: ", y)
-            vision_nt.putNumber('target_x', x)
-            vision_nt.putNumber('target_y', y)
+                    # Draw center circle
+                    cv2.circle(output_img, center=(x, y), radius=3, color=(0, 0, 255), thickness=-1)
 
-            # Draw rectangle and center circle on output image
-            rect = cv2.minAreaRect(contour)
-            cv2.drawContours(output_img, [cv2.boxPoints(rect).astype(int)], -1, color=(0, 0, 255), thickness=2)
-            cv2.circle(output_img, center=(x, y), radius=3, color=(0, 0, 255), thickness=-1)
+                    # Draw rectangle on output image
+                    # rect = cv2.minAreaRect(contour)
+                    # cv2.drawContours(output_img, [cv2.boxPoints(rect).astype(int)], -1, color=(0, 0, 255), thickness=2)
 
         # Time passed since start of frame and calculate FPS
         # processing_time = time.time() - start_time
@@ -83,6 +76,5 @@ def main():
 
         # Put output image on dashboard
         output_stream.putFrame(output_img)
-
-
+        print("active")
 main()
