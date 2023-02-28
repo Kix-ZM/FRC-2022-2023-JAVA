e827d4508c6dca34a1281c8eb03b306800aed4a6
HsSteven <256787@pylusd.org>
2023-02-28 01:08:46 -0600

    TurnAngle Update
    Improved Math/Logic behind how Turn Angle works. Should now fix the problem where it goes 90 degrees, then 270 degrees, and repeats

--- Mallet/src/main/java/frc/robot/commands/TurnAngle.java
+++ Mallet/src/main/java/frc/robot/commands/TurnAngle.java
@@ -11,7 +11,7 @@ public class TurnAngle extends CommandBase{
     private float startDes;
     private float turnDestination;
     private float m_turnAmount;
-    private boolean isf;
+    private boolean m_isTurnningBy;
     private int direction;
     private float errorRange = 5.0f;
     private boolean status;
@@ -21,13 +21,14 @@ public class TurnAngle extends CommandBase{
     isTurnningBy - wether or not the turnAmount is a destination, or we the amount we are adding to
     our current angle*/
     public TurnAngle(Drivetrain drivetrain, GyroScope gyro, float turnAmount, boolean isTurnningBy){
-        m_drivetrain = drivetrain;
-        m_gyro = gyro;
         System.out.println("\n==============================LoL==============================\n");
 
+        m_drivetrain = drivetrain;
+        m_gyro = gyro;
         addRequirements(m_drivetrain);
         addRequirements(m_gyro);
-        isf = isTurnningBy;
+
+        m_isTurnningBy = isTurnningBy;
         status = false;
         // Getting what the angle should be at the end
         // m_turnAmount = turnAmount;
@@ -37,15 +38,18 @@ public class TurnAngle extends CommandBase{
     @Override
     public void initialize() {
         System.out.println("Init");
-        startDes = m_gyro.getAngleZ();
-        if(startDes < 0){
-            startDes = 360 + startDes;
-        }
-
         status = false;
-        // if(isf){
-            m_turnAmount = startDes + 90.0f;
+        
+        m_turnAmount = 90.0f;
+
+        // if(m_isTurnningBy){
+            //Converts gyro to 360 degrees
+            startDes = (m_gyro.getAngleZ()+360.0f)%360.0f;
+            m_turnAmount += startDes;
         // }
+
+        //Converts 360 degrees back to gyro
+        m_turnAmount %= 360.0f;
         if(m_turnAmount > 180.0f){
             m_turnAmount = -1*(180-(m_turnAmount%180));
         }
@@ -60,9 +64,7 @@ public class TurnAngle extends CommandBase{
         /*If statements to check the distance it needs to go and if it is within a small margin or not
         and based of that, checks to see the direction to turn to (if it has already reached its destination,
         it will not move*/
-        if(m_turnAmount-m_gyro.getAngleZ()>errorRange){
-            direction = 1;
-        }else if(m_turnAmount-m_gyro.getAngleZ() < -1*errorRange){
+        if((m_turnAmount-m_gyro.getAngleZ()>errorRange) || m_turnAmount-m_gyro.getAngleZ() < -1*errorRange){
             direction = 1;
         }else{
             direction = 0;
