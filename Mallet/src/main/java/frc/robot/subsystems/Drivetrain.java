c65b7e75aa9623871475d803fce86909fe9cf726
tscullin <tommyscullin@gmail.com>
2023-03-01 16:03:58 -0800

    Revert "Drivetrain Update"
    This reverts commit 7ec4c5d79e51e542d09fab6f5bf4f9797a3e5c4a.

--- Mallet/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ Mallet/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -26,28 +26,23 @@ public class Drivetrain extends SubsystemBase {
   // private final Spark m_leftMotor = new Spark(0);
   //private final Spark m_rightMotor = new Spark(1);
 // 1 motor claw, 1 motor extnsion, 2 motors pivot
-  private final CANSparkMax m_flMotor;
-  private final CANSparkMax m_blMotor;
-  MotorControllerGroup m_left;
+  private final CANSparkMax m_flMotor = new CANSparkMax(3, MotorType.kBrushless);
+  private final CANSparkMax m_blMotor = new CANSparkMax(4, MotorType.kBrushless);
+  MotorControllerGroup m_left = new MotorControllerGroup(m_flMotor, m_blMotor);
   
-  private final CANSparkMax m_frMotor;
-  private final CANSparkMax m_brMotor;
-  MotorControllerGroup m_right;
+  private final CANSparkMax m_frMotor = new CANSparkMax(1, MotorType.kBrushless);
+  private final CANSparkMax m_brMotor = new CANSparkMax(2, MotorType.kBrushless);
+  MotorControllerGroup m_right = new MotorControllerGroup(m_frMotor, m_brMotor);
   
   // The Romi has onboard encoders that are hardcoded
   // to use DIO pins 4/5 and 6/7 for the left and right
-  private final RelativeEncoder m_leftEncoder;
-  private final RelativeEncoder m_rightEncoder;
-  private final RelativeEncoder m_leftBackEncoder;
-  private final RelativeEncoder m_rightBackEncoder;
+  private final RelativeEncoder m_leftEncoder = m_flMotor.getEncoder();
+  private final RelativeEncoder m_rightEncoder = m_frMotor.getEncoder();
+  private final RelativeEncoder m_leftBackEncoder = m_blMotor.getEncoder();
+  private final RelativeEncoder m_rightBackEncoder = m_brMotor.getEncoder();
 
   // Set up the differential drive controller
   private final DifferentialDrive m_diffDrive = new DifferentialDrive(m_left, m_right);
-
-  private final boolean isUsingFLMotor = true;
-  private final boolean isUsingFRMotor = true;
-  private final boolean isUsingBLMotor = true;
-  private final boolean isUsingBRMotor = true;
  // private final DifferentialDrive m_diffDrive = new DifferentialDrive(m_brMotor, m_frMotor);
   
 
@@ -60,45 +55,6 @@ public class Drivetrain extends SubsystemBase {
 
   /** Creates a new Drivetrain. */
   public Drivetrain() {
-    //Setup for motors and encoders
-    if(isUsingBLMotor){
-      m_blMotor = new CANSparkMax(4, MotorType.kBrushless);
-      m_leftBackEncoder = m_blMotor.getEncoder();
-    } 
-    if(isUsingFLMotor){
-      m_flMotor = new CANSparkMax(3, MotorType.kBrushless);
-      m_leftEncoder = m_flMotor.getEncoder();
-    }
-    if(isUsingBRMotor){
-      m_brMotor = new CANSparkMax(2, MotorType.kBrushless);
-      m_rightBackEncoder = m_brMotor.getEncoder();
-    } 
-    if(isUsingFRMotor){
-      m_frMotor = new CANSparkMax(1, MotorType.kBrushless);
-      m_rightEncoder = m_frMotor.getEncoder();
-    }
-
-    //Setup for motor groups
-    if(isUsingBLMotor){
-      if(isUsingFLMotor){
-        m_left = new MotorControllerGroup(m_flMotor, m_blMotor);
-      }else{
-        m_left = new MotorControllerGroup(m_blMotor);
-      }
-    }else if(isUsingFLMotor){
-      m_left = new MotorControllerGroup(m_flMotor);
-    }
-
-    if(isUsingBRMotor){
-      if(isUsingFRMotor){
-        m_right = new MotorControllerGroup(m_frMotor, m_brMotor);
-      }else{
-        m_right = new MotorControllerGroup(m_brMotor);
-      }
-    }else if(isUsingFRMotor){
-      m_right = new MotorControllerGroup(m_frMotor);
-    }
-
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
@@ -122,9 +78,6 @@ public class Drivetrain extends SubsystemBase {
   }
 
   public void arcadeDrive(double xaxisSpeed, double zaxisRotate) {
-    if(xaxisSpeed > Constants.maxSpeed){
-      xaxisSpeed = Constants.maxSpeed;
-    }
     m_diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate);
   }
 
@@ -136,9 +89,6 @@ public class Drivetrain extends SubsystemBase {
   }
 
   public void runTest(double speed){
-    if(speed > Constants.maxSpeed){
-      speed = Constants.maxSpeed;
-    }
     m_brMotor.set(speed);
   }
   // FOr when the time calls for it, run this
@@ -147,73 +97,43 @@ public class Drivetrain extends SubsystemBase {
   }
 
   public RelativeEncoder getLeftEncoder() {
-    if(isUsingFLMotor){
-      return m_leftEncoder;
-    }
-    return null;
+    return m_leftEncoder;
   }
 
   public RelativeEncoder getRightEncoder() {
-    if(isUsingFRMotor){
-      return m_rightEncoder;
-    }
-    return null;
+    return m_rightEncoder;
   }
 
   public RelativeEncoder getRightBackEncoder(){
-    if(isUsingBRMotor){
-      return m_rightBackEncoder;
-    }
-    return null;
+    return m_rightBackEncoder;
   }
 
   public RelativeEncoder getLeftBackEncoder(){
-    if(isUsingBLMotor){
-      return m_leftBackEncoder;
-    }
-    return null;
+    return m_leftBackEncoder;
   }
 
   public double getLeftDistanceInch() {
-    if(isUsingFLMotor){
-      return m_leftEncoder.getPosition();
-    }
-    return 0.0;
+    return m_leftEncoder.getPosition();
   }
 
   public double getLeftBackDistanceInch(){
-    if(isUsingBLMotor){
-      return m_leftBackEncoder.getPosition();
-    }
-    return 0.0;
+    return m_leftBackEncoder.getPosition();
   }
 
   public double getRightDistanceInch() {
-    if(isUsingFRMotor){
-      return m_rightEncoder.getPosition();
-    }
-    return 0.0;
+    return m_rightEncoder.getPosition();
   }
 
   public double getRightBackDistanceInch(){
-    if(isUsingBRMotor){
-      return m_rightBackEncoder.getPosition();
-    }
-    return 0.0;
+    return m_rightBackEncoder.getPosition();
   }
 
   public double getLeftAverageDistanceInch(){
-    if(isUsingBLMotor && isUsingBRMotor){
-      return ((getLeftDistanceInch()) + (getLeftBackDistanceInch())) / 2.0;
-    }
-    return getLeftDistanceInch() + getLeftBackDistanceInch();
+    return ((getLeftDistanceInch()) + (getLeftBackDistanceInch())) / 2.0;
   }
 
   public double getRightAverageDistanceInch(){
-    if(isUsingBRMotor && isUsingFRMotor){
-      return ((getRightDistanceInch()) + (getRightBackDistanceInch())) / 2.0;
-    }
-    return ((getRightDistanceInch()) + (getRightBackDistanceInch()));
+    return ((getRightDistanceInch()) + (getRightBackDistanceInch())) / 2.0;
   }
 
   public double getAverageDistanceInch() {
