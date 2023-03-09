59e4f7fbea3dc137c298f23db1bbaaf15c5532cb
overlordpro-sys <64398319+overlordpro-sys@users.noreply.github.com>
2023-03-09 12:21:43 -0800

    Made it so AutoBalance works from other side too

--- Mallet/src/main/java/frc/robot/commands/AutoBalance.java
+++ Mallet/src/main/java/frc/robot/commands/AutoBalance.java
@@ -13,6 +13,7 @@ public class AutoBalance extends CommandBase {
   private final Drivetrain m_drivetrain;
   private boolean startBalancing;
   private boolean onPlatform;
+  private int backwardsScaler;
   /**
    * Creates a new AutoBalance. This command balances the robot on the charging station. 
    * THIS WILL ONLY WORK IF THE ROBOT'S Y ANGLE STARTS OFF NEGATIVE (IMU orientation)
@@ -21,9 +22,11 @@ public class AutoBalance extends CommandBase {
    * @param drivetrain The drivetrain subsystem on which this command will run
    * @param gyro The gyro subsystem on which this command will run
    */
-  public AutoBalance(Drivetrain drivetrain, GyroScope gyro) {
+  public AutoBalance(Drivetrain drivetrain, GyroScope gyro, boolean backwards) {
     startBalancing = false;
     onPlatform = false;
+    backwardsScaler = backwards ? -1 : 1; 
+
     m_gyro = gyro;
     m_drivetrain = drivetrain;
     addRequirements(gyro);
@@ -43,13 +46,13 @@ public class AutoBalance extends CommandBase {
     double pitchAngleDegrees = m_gyro.getAngleY();
     if (!startBalancing)
     {
-      m_drivetrain.arcadeDrive(Constants.K_FWD_SPEED, 0);
+      m_drivetrain.arcadeDrive(backwardsScaler*Constants.K_FWD_SPEED, 0);
       // if at high enough angle so we know it is on the platform
       if (Math.abs(pitchAngleDegrees) > Constants.K_PLAT_DEGREE_THRESH)
         onPlatform = true;
       
       // if on platform and tilted to the other side of the charger
-      if (onPlatform && pitchAngleDegrees > 0) 
+      if (onPlatform && (backwardsScaler == 1 ? pitchAngleDegrees > 0 : pitchAngleDegrees < 0)) 
         startBalancing = true;
     }
     else {
--- Mallet/src/main/java/frc/robot/commands/AutoGroups/AutoGroup_LeaveCommAndBalance.java
+++ Mallet/src/main/java/frc/robot/commands/AutoGroups/AutoGroup_LeaveCommAndBalance.java
@@ -4,6 +4,7 @@ import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.GyroScope;
 import frc.robot.commands.ResetEncoders;
 import frc.robot.commands.AutoBalance;
+import frc.robot.commands.MoveDistance;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class AutoGroup_LeaveCommAndBalance extends SequentialCommandGroup {
@@ -15,8 +16,9 @@ public class AutoGroup_LeaveCommAndBalance extends SequentialCommandGroup {
         addCommands(
             new ResetEncoders(drivetrain),
 
+            new MoveDistance(drivetrain, 5, false),
             // balance
-            new AutoBalance(drivetrain, gyro)
+            new AutoBalance(drivetrain, gyro, true)
 
         );
     }
