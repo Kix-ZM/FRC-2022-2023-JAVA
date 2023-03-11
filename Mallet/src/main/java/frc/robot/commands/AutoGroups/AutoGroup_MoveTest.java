05574641d51165e0456d6dc4ebc5925889dd7faa
tscullin <tommyscullin@gmail.com>
2023-03-11 13:41:41 -0800

    Update MoveDistanceV2
    Update speed values to make it work

--- Mallet/src/main/java/frc/robot/commands/AutoGroups/AutoGroup_MoveTest.java
+++ Mallet/src/main/java/frc/robot/commands/AutoGroups/AutoGroup_MoveTest.java
@@ -3,21 +3,21 @@ package frc.robot.commands.AutoGroups;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.GyroScope;
 import frc.robot.commands.ResetEncoders;
-import frc.robot.commands.AutoBalance;
-import frc.robot.commands.MoveDistance;
+import frc.robot.Constants;
+import frc.robot.commands.MoveDistanceV2;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class AutoGroup_MoveTest extends SequentialCommandGroup {
     //Variables
-    Drivetrain m_Drivetrain;
+    private Drivetrain m_Drivetrain;
     public AutoGroup_MoveTest(Drivetrain drivetrain, GyroScope gyro){
         //Adding a drivetrain
         m_Drivetrain = drivetrain;
         //Adding Order of commands
         addCommands(
             new ResetEncoders(this.m_Drivetrain),
-            new MoveDistance(this.m_Drivetrain, 10, false),
-            new MoveDistance(this.m_Drivetrain, 2, false)
+            new MoveDistanceV2(this.m_Drivetrain, 5 * Constants.K_TICKS_PER_FEET, false)
+            //new MoveDistanceV2(this.m_Drivetrain, 2, false)
         );
     }
 }
--- Mallet/src/main/java/frc/robot/commands/MoveDistanceV2.java
+++ Mallet/src/main/java/frc/robot/commands/MoveDistanceV2.java
@@ -5,27 +5,32 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 public class MoveDistanceV2 extends CommandBase{
     private Drivetrain m_Drivetrain;
     private double endDistance;
+    private double m_newFeet;
     
     public MoveDistanceV2(Drivetrain drivetrain, double newFeet, boolean backwards){
-        m_Drivetrain.resetEncoders();
-        endDistance = m_Drivetrain.getAverageDistanceInch() + newFeet*12;
+        System.out.println("constructor");
         m_Drivetrain  = drivetrain;
+        m_Drivetrain.resetEncoders();
+        m_newFeet = newFeet;
+        endDistance = m_Drivetrain.getAverageDistanceInch() + newFeet;
         addRequirements(drivetrain);
     }
     
     public void initialize() {
-        
-    }
-    public void execute() {
-        m_Drivetrain.arcadeDrive(.5, 0);
+        System.out.println("initialize");
+        m_Drivetrain.resetEncoders();
+        endDistance = m_Drivetrain.getAverageDistanceInch() + m_newFeet;
     }
 
-        
+    public void execute() {
+        m_Drivetrain.arcadeDrive(.4, 0);
+    }
     
     public void end(boolean interrupted){
         m_Drivetrain.arcadeDrive(0.0, 0.0);
     }
     public boolean isFinished(){
-        return Math.abs(m_Drivetrain.getAverageDistanceInch() - endDistance) < 1;
+        //return Math.abs(m_Drivetrain.getAverageDistanceInch() - endDistance) < 1;
+        return endDistance < m_Drivetrain.getAverageDistanceInch();
     }
 }
