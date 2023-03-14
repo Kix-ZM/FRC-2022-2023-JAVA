a8b5c26db373f7f4fb0ca4cdaa0f9a8a1562581c
TerminatingCoder101 <ganesha4us1@gmail.com>
2023-03-14 10:17:58 -0700

    Update on Leave Community haven't checked yet

--- Mallet/src/main/java/frc/robot/commands/AutoGroups/AutoGroup_LeaveCommAndBalance.java
+++ Mallet/src/main/java/frc/robot/commands/AutoGroups/AutoGroup_LeaveCommAndBalance.java
@@ -1,26 +1,34 @@
 package frc.robot.commands.AutoGroups;
-
+import frc.robot.Constants;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.GyroScope;
-import frc.robot.commands.ResetEncoders;
+// import frc.robot.commands.ResetEncoders;
 import frc.robot.commands.AutoBalance;
-import frc.robot.commands.MoveDistance;
-import frc.robot.Constants;
+//import frc.robot.commands.MoveDistance;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
 
 public class AutoGroup_LeaveCommAndBalance extends SequentialCommandGroup {
     //Variables
     public AutoGroup_LeaveCommAndBalance(Drivetrain drivetrain, GyroScope gyro){
         //Adding a drivetrain
         //Adding Order of commands
+        double pitchAngleDegrees = gyro.getAngleY();
 
-        addCommands(
-            new ResetEncoders(drivetrain),
+        drivetrain.resetEncoders();
+        drivetrain.arcadeDrive(Constants.K_FWD_SPEED, 0);
+
+        if (Math.abs(pitchAngleDegrees) == 0) //Math.abs(pitchAngleDegrees) > 0 && Math.abs(pitchAngleDegrees) < 1
+            new AutoBalance(drivetrain,gyro,true);
+
+
+        // addCommands(
+        //     new ResetEncoders(drivetrain),
             
-            new MoveDistance(drivetrain, 18, false),
-            // balance
-            new AutoBalance(drivetrain, gyro, true)
+        //     new MoveDistance(drivetrain, 18, false),
+        //     // balance
+        //     new AutoBalance(drivetrain, gyro, true)
 
-        );
+        // );
     }
 }
