bf678a49b2659b31d66719d68d06cfb61059267c
tscullin <tommyscullin@gmail.com>
2023-03-14 16:33:25 -0700

    Arcade Drive Bug Fix
    Arcade Drive used to have choppy increases in speed. The error was that when getting the time, it was instantly divided by 1000 to convert to seconds. This resulted in some roundoff and thus the speed only changed per seconds. Code now fix for smoother increase in speed.

--- Mallet/src/main/java/frc/robot/commands/ArcadeDrive.java
+++ Mallet/src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -22,10 +22,12 @@ public class ArcadeDrive extends CommandBase {
   @Override
   public void execute() {
     //Checks if the joystick is not active in order to update startTime
-    if (Math.abs(m_joystick.getRawAxis(1)) < 0.1)
-      startTime = System.currentTimeMillis() / 1000;
-    currentTime = System.currentTimeMillis() / 1000;
-    //System.out.println(getSpeed(-m_joystick.getRawAxis(1)));
+    if (Math.abs(m_joystick.getRawAxis(1)) < 0.1){
+      startTime = System.currentTimeMillis();
+      currentTime = startTime;
+    }else{
+      currentTime = System.currentTimeMillis();
+    }
     m_drivetrain.arcadeDrive(getSpeed(-m_joystick.getRawAxis(1)), -m_joystick.getRawAxis(0));
   }
 
@@ -43,8 +45,9 @@ public class ArcadeDrive extends CommandBase {
     Uses an exponential function (can plug this into desmos): 
     f(x) = 2^(x/3)-0.8
     *0.8 is used to get the x-int of 0.2 in order to have a dead zone as any speed below 0.1 doesn't work
+    *3000 is 3 is time in seconds to max speed (aka 1 although it is not exactly at 3) and the 1000 is for the converting to milliseconds
     */
-    double speed = (Math.pow(2, (currentTime - startTime) / 3) - .8) * xaxisSpeed / Math.abs(xaxisSpeed);
+    double speed = (Math.pow(2, (currentTime - startTime) / 3000) - .8) * Math.signum(xaxisSpeed);
     //Prevents the speed from going over the given joystick speed both negative and positibe
     if (Math.abs(speed) > Math.abs(xaxisSpeed))
       speed = xaxisSpeed;
