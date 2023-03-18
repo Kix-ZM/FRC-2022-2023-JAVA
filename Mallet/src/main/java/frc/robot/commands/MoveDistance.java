ec14a979d7bbe9d03e164e5531bfed1b1de4336b
dragi <115513503+dragi@users.noreply.github.com>
2023-03-17 18:24:12 -0700

    Update MoveDIstance
    made the backwards boolean actually do stuff

--- Mallet/src/main/java/frc/robot/commands/MoveDistance.java
+++ Mallet/src/main/java/frc/robot/commands/MoveDistance.java
@@ -8,24 +8,35 @@ public class MoveDistance extends CommandBase{
     private Drivetrain m_Drivetrain;
     private double endDistance;
     private double m_newFeet;
+    private boolean m_isBackwards;
     
     public MoveDistance(Drivetrain drivetrain, double newFeet, boolean backwards){
         System.out.println("constructor");
+        m_isBackwards = backwards;
         m_Drivetrain  = drivetrain;
         m_Drivetrain.resetEncoders();
         m_newFeet = newFeet * Constants.K_TICKS_PER_FEET;
-        endDistance = m_Drivetrain.getAverageDistanceInch() + newFeet;
+        if(!m_isBackwards)
+            endDistance = m_Drivetrain.getAverageDistanceInch() + newFeet;
+        else    
+            endDistance = m_Drivetrain.getAverageDistanceInch() - newFeet;
         addRequirements(drivetrain);
     }
     
     public void initialize() {
         System.out.println("initialize");
         m_Drivetrain.resetEncoders();
-        endDistance = m_Drivetrain.getAverageDistanceInch() + m_newFeet;
+        if(!m_isBackwards)
+            endDistance = m_Drivetrain.getAverageDistanceInch() + m_newFeet;
+        else 
+            endDistance = m_Drivetrain.getAverageDistanceInch() - m_newFeet;
     }
 
     public void execute() {
-        m_Drivetrain.arcadeDrive(.4, 0);
+        if(!m_isBackwards)
+            m_Drivetrain.arcadeDrive(.4, 0);
+        else   
+            m_Drivetrain.arcadeDrive(-0.4, 0);
     }
     
     public void end(boolean interrupted){
@@ -33,6 +44,9 @@ public class MoveDistance extends CommandBase{
     }
     public boolean isFinished(){
         //return Math.abs(m_Drivetrain.getAverageDistanceInch() - endDistance) < 1;
-        return endDistance < m_Drivetrain.getAverageDistanceInch();
+        if(!m_isBackwards)
+            return endDistance < m_Drivetrain.getAverageDistanceInch();
+        else
+            return endDistance > m_Drivetrain.getAverageDistanceInch();
     }
 }
